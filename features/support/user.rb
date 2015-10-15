b129533d1954f36cbbf7798bb6b099216be6d6ec
davelpat <dpatterson@monsooncommerce.com>
2015-10-15 15:43:01 -0700

    Replaced User initializer with a superclass, BaseTestObject, to conatain common initializer and methods for test classes

--- features/support/user.rb
+++ features/support/user.rb
@@ -1,10 +1,2 @@
-class User
-  attr_accessor :name, :screen_name, :service, :user_id
-
-  def initialize(args)
-    @name        = args[:name]
-    @screen_name = args[:screen_name]
-    @service     = args[:service]
-    @user_id     = args[:user_id]
-  end
+class User < BaseTestObject
 end
