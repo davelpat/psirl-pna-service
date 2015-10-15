b87d46742928558ae6b5998a36b6e31c4c8fb0fd
davelpat <dpatterson@monsooncommerce.com>
2015-10-15 12:12:42 -0700

    Added initial step defs

---
+++ features/step_defintions/twitter_rest_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I access the resource url "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I retrieve the results$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the status code should be (\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it should have the field "([^"]*)" containing the value "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
