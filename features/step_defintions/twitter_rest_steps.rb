11a3e73d4de9da74558acda44e72b64b9522f4ff
davelpat <dpatterson@monsooncommerce.com>
2015-10-15 12:40:47 -0700

    abstracted twitter_user.feature to use business language

--- features/step_defintions/twitter_rest_steps.rb
+++ features/step_defintions/twitter_rest_steps.rb
@@ -13,3 +13,15 @@ end
 Then(/^it should have the field "([^"]*)" containing the value "([^"]*)"$/) do |arg1, arg2|
   pending # Write code here that turns the phrase above into concrete actions
 end
+
+Given(/^a (\w+) user whose screen name is (\w+)$/) do |service, screen_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I get their user info$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it should include their ([\w\s]+) and (\d+)$/) do |full_name, user_id|
+  pending # Write code here that turns the phrase above into concrete actions
+end
--- features/twitter_user.feature
+++ features/twitter_user.feature
@@ -7,3 +7,13 @@ Feature: Show twitter user info
     Then the status code should be 200
     And it should have the field "name" containing the value "Jason Harmon"
     And it should have the field "id" containing the value "57005215"
+
+  @Positive @Get @User
+  Scenario Outline: Show twitter user info
+    Given a twitter user whose screen name is <screen name>
+    When I get their user info
+    Then it should include their <name> and <id>
+
+    Examples:
+      | name         | screen name     | id       |
+      | Jason Harmon | jasonh_n_austin | 57005215 |
